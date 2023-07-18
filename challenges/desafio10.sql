395649b16942fc959a81b217c2f05b186eec7097
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-07-18 05:06:18 -0300

    requisito 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,31 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE cancoes_favoritas (
+  musica_id INT,
+  pessoa_usuaria_id INT,
+  PRIMARY KEY (musica_id, pessoa_usuaria_id),
+  FOREIGN KEY (musica_id) REFERENCES musica(musica_id),
+  FOREIGN KEY (pessoa_usuaria_id) REFERENCES pessoa_usuaria(pessoa_usuaria_id)
+) ENGINE = InnoDB;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO cancoes_favoritas (musica_id, pessoa_usuaria_id)
+VALUES
+  -- Barbara Liskov
+  (3, 1), -- ALIEN SUPERSTAR
+  (5, 1), -- Como Nossos Pais
+  (10, 1), -- Feeling Good
+  -- Robert Cecil Martin
+  (4, 2), -- Don't Stop Me Now
+  -- Ada Lovelace
+  (1, 3), -- BREAK MY SOUL
+  (3, 3), -- ALIEN SUPERSTAR
+  -- Martin Fowler
+  (6, 4), -- O Medo de Amar é o Medo de Ser Livre
+  (4, 4), -- Don't Stop Me Now
+  -- Sandi Metz
+  (10, 5), -- Feeling Good
+  (2, 5), -- VIRGO'S GROOVE
+  -- Christopher Alexander
+  (4, 6), -- Don't Stop Me Now
+  -- Judith Butler
+  (6, 7), -- O Medo de Amar é o Medo de Ser Livre
+  -- Jorge Amado
+  (3, 8); -- ALIEN SUPERSTAR
